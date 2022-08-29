cb6c1622787449c0c459108028a665b9d7b627d1
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-29 15:51:27 +0530

    The set method is created and display book method is created

--- src/main/java/com/niit/jdp/Book.java
+++ src/main/java/com/niit/jdp/Book.java
@@ -13,5 +13,41 @@ public class Book {
     public int price;
     public String yearOfPublication;
 
+    public void setIsbnNumber(String isbnNumber) {
+        this.isbnNumber = isbnNumber;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setYearOfPublication(String yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+
+    public String displayBookDetail() {
+        String isbn = "23456";
+        String title = "Do your self";
+        String description = "Mind your own business";
+        int price = 499;
+        String yearOfPublication = "2022";
+        Author author;
+        String authorName = "Meshach";
+        String authorThoughts = "unknown";
+        String authorFavourites = "useless";
+
+        return displayBookDetail();
+    }
+
+}
 
 
